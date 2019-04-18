6f405f1ffba14cd11f60a17ac6eba45c62fc2187
Xingeng Zhang <zhangxingeng970221@gmail.com>
2019-04-18 19:04:20 -0400

    geng

--- src/data/Admin_data.java
+++
@@ -1,9 +0,0 @@
-package data;
-import data.Account_data;
-
-public class Admin_data extends Account_data{
-	
-	public Admin_data(String email, String password) {
-		super(email,password,"admin");
-	}
-}

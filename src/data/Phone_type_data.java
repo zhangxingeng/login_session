144913c4dc8404ba1ddf6a54ad4612b27690de72
Haoran Wen <haoranwen1@gmail.com>
2019-04-20 11:11:07 -0400

    ff

--- src/data/Phone_type_data.java
+++ src/data/Phone_type_data.java
@@ -1,6 +1,5 @@
 package data;
 
-public class Phone_type_data {
 	private String brand;
 	private String model;
 	private int ram;
@@ -8,21 +7,16 @@ public class Phone_type_data {
 	private int cpu_core;
 	private String os;
 	
-	
-	public Phone_type_data(String brand, String model, int ram, int rom, int cpu_core, String os) {
-		super();
+		
 		this.brand = brand;
 		this.model = model;
 		this.ram = ram;
 		this.rom = rom;
 		this.cpu_core = cpu_core;
-		this.os = os;
 	}
-	
 	public String getBrand() {
 		return brand;
 	}
-	public void setBrand(String brand) {
 		this.brand = brand;
 	}
 	public String getModel() {
@@ -37,13 +31,11 @@ public class Phone_type_data {
 	public void setRam(int ram) {
 		this.ram = ram;
 	}
-	public int getRom() {
 		return rom;
 	}
 	public void setRom(int rom) {
 		this.rom = rom;
 	}
-	public int getCpu_core() {
 		return cpu_core;
 	}
 	public void setCpu_core(int cpu_core) {
@@ -55,6 +47,5 @@ public class Phone_type_data {
 	public void setOs(String os) {
 		this.os = os;
 	}
-
 	
 }

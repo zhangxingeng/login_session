69c6943ebf2d4836503585d69530be943a90db77
Haoran Wen <haoranwen1@gmail.com>
2019-04-20 15:03:40 -0400

    Update Seller_handler.java
    hw

--- src/request_handler/Seller_handler.java
+++ src/request_handler/Seller_handler.java
@@ -14,17 +14,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import connect.DBConnect;
-
-
-
 
 
 

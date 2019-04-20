cdd31c339906559131e00a53642ce09cf51a232c
Xingeng Zhang <zhangxingeng970221@gmail.com>
2019-04-20 18:42:50 -0400

    g

---
+++ src/request_handler/Removeauction_handler.java
@@ -0,0 +1,27 @@
+package request_handler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Removeauction_handler")
+public class Removeauction_handler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Removeauction_handler() {
+        super();
+    }
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/request_handler/Removebid_handler.java
@@ -0,0 +1,27 @@
+package request_handler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Removebid_handler")
+public class Removebid_handler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Removebid_handler() {
+        super();
+    }
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		
+		
+	}
+
+}

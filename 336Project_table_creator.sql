585cec486ff692e8008cf585329b972b0cadf33a
Haoran Wen <haoranwen1@gmail.com>
2019-04-21 13:13:01 -0400

    Update 336Project_table_creator.sql

--- 336Project_table_creator.sql
+++ 336Project_table_creator.sql
@@ -335,9 +335,6 @@ CREATE TABLE watchlist(
 );
 
 CREATE TABLE Email(
-	messageId INT AUTO_INCREMENT,
-	to_username VARCHAR(50),
-    from_username VARCHAR(50),
 	message_Id INT AUTO_INCREMENT,
 	to_email VARCHAR(50),
     from_email VARCHAR(50),

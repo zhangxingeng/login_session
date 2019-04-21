33420cc2077a0dd4d8402a0b1017c97c2df4a5a7
krisgao <zhushiwoliliang@126.com>
2019-04-21 18:56:03 -0400

    zg121

--- src/request_handler/Item_detail_handler.java
+++ src/request_handler/Item_detail_handler.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.util.*;
 import java.sql.*;
 import javax.servlet.*;

484a3564ab2d0ec789dc53cc44139b7487330adf
Haoran Wen <haoranwen1@gmail.com>
2019-04-21 22:30:03 -0400

    g
    hr

--- src/data/List_answer_data.java
+++ src/data/List_answer_data.java
@@ -1,24 +1,15 @@
 package data;
 
 public class List_answer_data {
-	private int question_num;
 	private String email;
 	private String answer;
 	
 	
 	public List_answer_data(int question_num, String email, String answer) {
-		super();
-		this.question_num = question_num;
 		this.email = email;
 		this.answer = answer;
 	}
 	
-	public int getQuestion_num() {
-		return question_num;
-	}
-	public void setQuestion_num(int question_num) {
-		this.question_num = question_num;
-	}
 	public String getEmail() {
 		return email;
 	}
